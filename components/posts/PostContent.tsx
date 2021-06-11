c776b3c674a1a663b0bdf26b9bf85474963ddfc2
Pxlart Design <michael.rhema@gmail.com>
2021-06-11 10:39:27 +0000

    - Fixing media and embeded content rendering

--- components/posts/PostContent.tsx
+++ components/posts/PostContent.tsx
@@ -1,26 +1,55 @@
 import React from "react";
-import parse, { attributesToProps, HTMLReactParserOptions } from 'html-react-parser';
+import parse, {
+  attributesToProps,
+  DOMNode,
+  HTMLReactParserOptions,
+} from "html-react-parser";
 
-export function PostContent(props: {
-  content: string
-}) {
-  const options: HTMLReactParserOptions = {
-    replace: (domDode) => {
-      const {attribs} = domDode
+const MediaElement = (ele: DOMNode) => {
+  const { attribs, name } = ele;
+
+  const media = {
+    img: (attribs) => {
       if (attribs && attribs.src && attribs.src.length > 0) {
         const props = attributesToProps(attribs);
-        props.src = `${props.src.replace(process.env.NEXT_PUBLIC_API_HOST, process.env.NEXT_PUBLIC_MEDIA_HOST)} `  //<name>?tr=w-1200,h-630,fo-auto
+        props.src = `${props.src.replace(
+          process.env.NEXT_PUBLIC_API_HOST,
+          process.env.NEXT_PUBLIC_MEDIA_HOST
+        )} `; //<name>?tr=w-1200,h-630,fo-auto
         return <img {...props} />;
       }
-    }
-  }
-  return (
-    <div
-      className="post-full-content"
-    >
-      {
-        parse(props.content, options)
+    },
+    a: (attribs) => {
+      if (attribs && attribs.href && attribs.href.length > 0 && attribs.href.indexOf('.mp4') > -1) {
+        const props = attributesToProps(attribs);
+        const src = `${props.href.replace(
+          process.env.NEXT_PUBLIC_API_HOST,
+          process.env.NEXT_PUBLIC_MEDIA_HOST
+        )} `;
+        return (
+          <video
+          width="100%"
+          autoPlay
+          loop
+            children={
+              <source src={src} type="video/mp4" />
+            }
+          />
+        );
       }
-    </div>
+    },
+  };
+
+  return media[name] && media[name](attribs);
+};
+
+export function PostContent(props: { content: string }) {
+  const options: HTMLReactParserOptions = {
+    replace: (domDode) => {
+      return MediaElement(domDode);
+    },
+  };
+  return (
+    <div className="post-full-content">{parse(props.content, options)}</div>
   );
 }
--- pages/posts/index.jsx
+++ pages/posts/index.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
 import DefaultPageLayout from '../../components/layout/default'
 import PostArchive from '../../components/posts/archive'
-import { useRouter } from 'next/router'
-
 
 export default function Posts() {
-  const router = useRouter()
-
-
     return (
       <DefaultPageLayout title="All Posts" >
-        
         <PostArchive />
       </DefaultPageLayout>
     )
